bc421f1cded66c6cedc0b493aadefbd2835c65a5
bgreen33 <143014502+bgreen33@users.noreply.github.com>
2023-08-30 22:59:52 -0500

    Homework 1 upload

---
+++ Homework1 .cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //defining variables and equation
+            double X = 2.5;
+            double Y = 3.3;
+            double Z = 4 * X * X + 3 * Y;
+
+            //printing output
+            Console.WriteLine($"X = {X}, Y={Y}");
+            Console.WriteLine($"Thevalue of Z is:{Z}");
+        }
+    }
+}
